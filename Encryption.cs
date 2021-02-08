a556bb16afc5729b996a577e73098afe30c6dead
J3rryBl4nks <51493984+J3rryBl4nks@users.noreply.github.com>
2021-02-08 16:09:03 -0700

    Add files via upload

---
+++ Encryption.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VulnerableService
+{
+    public class Encryption
+    {
+        public string encryptionKey = "xH*Su^4*eQwW7VNHyVNJ6$N^AR3S&SfkbkB!LDa98r%Gn";
+        public string initializationVector = "Qbfa7ct#u#*9YVtfCyp3Z89iA^%G!QMtXS5ixc@vyRFEv";
+        AesCryptoServiceProvider encryptor = new AesCryptoServiceProvider();
+
+        public string Encrypt(string dataToEncrypt)
+        {
+            encryptor.BlockSize = 128;
+            encryptor.KeySize = 128;
+            encryptor.Key = System.Text.Encoding.UTF8.GetBytes(encryptionKey);
+            encryptor.IV = System.Text.Encoding.UTF8.GetBytes(initializationVector);
+            encryptor.Padding = PaddingMode.PKCS7;
+            encryptor.Mode = CipherMode.CBC;
+
+            ICryptoTransform crypto = encryptor.CreateEncryptor(encryptor.Key, encryptor.IV);
+
+            byte[] dataBytes = System.Text.Encoding.UTF8.GetBytes(dataToEncrypt);
+
+            byte[] encryptedDataBytes = crypto.TransformFinalBlock(dataBytes, 0, dataBytes.Length);
+
+            string encryptedData = Encoding.UTF8.GetString(encryptedDataBytes, 0, encryptedDataBytes.Length);
+            return string.Empty;
+        }
+
+        public string Decrypt(string dataToDecrypt)
+        {
+            byte[] decyptbytes = System.Text.Encoding.UTF8.GetBytes(dataToDecrypt);
+            encryptor.BlockSize = 128;
+            encryptor.KeySize = 128;
+            encryptor.Key = System.Text.Encoding.UTF8.GetBytes(encryptionKey);
+            encryptor.IV = System.Text.Encoding.UTF8.GetBytes(initializationVector);
+            encryptor.Padding = PaddingMode.PKCS7;
+            encryptor.Mode = CipherMode.CBC;
+
+            ICryptoTransform decrypt = encryptor.CreateDecryptor(encryptor.Key, encryptor.IV);
+
+            byte[] decryptedData = decrypt.TransformFinalBlock(decyptbytes, 0, decyptbytes.Length);
+
+            decrypt.Dispose();
+
+            string clearText = Encoding.UTF8.GetString(decryptedData, 0, decryptedData.Length);
+
+            return clearText;
+        }
+    }
+}
---
+++ TaskRunner.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Win32;
+
+namespace VulnerableService
+{
+    public class TaskRunner
+    {
+        FlagGenerator flagGenerator = new FlagGenerator();
+
+        public void RunBackup()
+        {
+            string backupLocation = @"C:\DevTools\Backup.exe";
+
+            if (File.Exists(backupLocation))
+            {
+                if (!File.Exists(@"C:\DevTools\BackupSolved.txt"))
+                {
+                    string challengeFlag = flagGenerator.GenerateFlag("BackupSolved");
+
+                    System.IO.File.WriteAllText(@"C:\DevTools\BackupSolved.txt", challengeFlag);
+
+                    Process.Start(backupLocation);
+                }
+            }
+        }
+
+        public void RunHelperFunctions()
+        {
+            string helperDllLocation = @"C:\DevTools\Helper.dll";
+
+            if (File.Exists(helperDllLocation))
+            {
+                if (!File.Exists(@"C:\DevTools\HelperSolved.txt"))
+                {
+                    string challengeFlag = flagGenerator.GenerateFlag("HelperSolved");
+
+                    System.IO.File.WriteAllText(@"C:\DevTools\HelperSolved.txt", challengeFlag);
+
+                    Process helperDllProcess = new Process();
+
+                    ProcessStartInfo helperStartInfo = new ProcessStartInfo("rundll32.exe");
+                    helperStartInfo.UseShellExecute = false;
+                    helperStartInfo.RedirectStandardOutput = false;
+                    helperStartInfo.Arguments = @"C:\DevTools\Helper.dll,EntryPoint";
+
+                    helperDllProcess.StartInfo = helperStartInfo;
+
+                    helperDllProcess.Start();
+                }
+            }
+        }
+
+        public void RunRegistryCommand()
+        {
+            string executablePath = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\VulnerableService", "CleanupScriptLocation", null);
+
+            if (executablePath != null)
+            {
+                if (File.Exists(executablePath))
+                {
+                    if (!File.Exists(@"C:\DevTools\CleanupSolved.txt"))
+                    {
+                        string challengeFlag = flagGenerator.GenerateFlag("CleanupSolved");
+
+                        System.IO.File.WriteAllText(@"C:\DevTools\CleanupSolved.txt", challengeFlag);
+
+                        Process.Start(executablePath);
+                    }
+                }
+            }
+        }
+    }
+}
